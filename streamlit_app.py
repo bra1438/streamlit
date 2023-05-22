877beca211dd366209d82b8cc0629b897653f337
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-22 13:34:58 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,36 @@
 import streamlit as st
 import requests
 
-API_URL = "https://api.openai.com/v1/chat/completions"
+# Streamlit app title
+st.title("ChatGPT API Demo")
 
-def get_chat_response(message):
+# ChatGPT API endpoint
+api_endpoint = "https://api.openai.com/v1/chat/completions"
+
+# OpenAI API key
+api_key = "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"
+
+# User input text field
+user_input = st.text_input("Enter your message:")
+
+# Send user input to the ChatGPT API
+if user_input:
     headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer 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"
+        "Authorization": f"Bearer {api_key}"
     }
-
-    data = {
+    payload = {
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": message}]
+        "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": user_input}]
     }
 
-    response = requests.post(API_URL, headers=headers, json=data)
-    response_json = response.json()
-    return response_json['choices'][0]['message']['content']
+    response = requests.post(api_endpoint, json=payload, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        chat_response = data["choices"][0]["message"]["content"]
 
-def main():
-    st.title("ChatGPT API Demo")
-    user_input = st.text_input("Enter your message")
-    
-    if user_input:
-        response = get_chat_response(user_input)
-        st.text_area("AI's Reply", value=response, height=200)
-    else:
-        st.info("Please enter a message to start the conversation.")
+        # Display the response from ChatGPT
+        st.text_area("ChatGPT:", value=chat_response, height=200)
 
-if __name__ == "__main__":
-    main()
+# Instructions for the user
+st.text("Enter a message, and the assistant will respond.")
