bf36b213231434566679e641a8b10e541475f316
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-22 13:31:42 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,32 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import requests
 
-"""
-# Welcome to Streamlit!
+API_URL = "https://api.openai.com/v1/chat/completions"
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def get_chat_response(message):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer YOUR_API_KEY"
+    }
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": message}]
+    }
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    response = requests.post(API_URL, headers=headers, json=data)
+    response_json = response.json()
+    return response_json['choices'][0]['message']['content']
 
+def main():
+    st.title("ChatGPT API Demo")
+    user_input = st.text_input("Enter your message")
+    
+    if user_input:
+        response = get_chat_response(user_input)
+        st.text_area("AI's Reply", value=response, height=200)
+    else:
+        st.info("Please enter a message to start the conversation.")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
