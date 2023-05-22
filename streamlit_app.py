0acfff5547dbcce8445bac2c932091f4c46d4164
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-22 13:39:40 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,52 @@
 import streamlit as st
 import requests
 
-# Define the API endpoint for ChatGPT
-API_ENDPOINT = 'https://api.openai.com/v1/chat/completions'
+API_URL = 'https://api.openai.com/v1/chat/completions'  # ChatGPT API URL
 
-# Set your OpenAI API key
+# Define your OpenAI API key
 API_KEY = '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'
 
-# Helper function to send a chat request to ChatGPT API
-def send_chat_request(message):
-    headers = {
-        'Authorization': 'Bearer ' + API_KEY,
-        'Content-Type': 'application/json'
-    }
+# Define the conversation history
+conversation = []
+
+# Send user input to the ChatGPT API and get the model's response
+def send_message(message):
     payload = {
-        'messages': [{'role': 'system', 'content': 'user'}, {'role': 'user', 'content': message}]
+        'messages': [
+            {'role': 'system', 'content': 'You are a user'},
+            {'role': 'user', 'content': message}
+        ]
     }
-    response = requests.post(API_ENDPOINT, json=payload, headers=headers)
-    return response.json()
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {API_KEY}'
+    }
 
-# Streamlit app code
+    response = requests.post(API_URL, json=payload, headers=headers)
+    data = response.json()
+    model_response = data['choices'][0]['message']['content']
+    
+    return model_response
+
+# Streamlit app
 def main():
-    st.title("ChatGPT Streamlit App")
-    st.write("Enter your message below:")
-
+    st.title('ChatGPT Streamlit Demo')
+    
+    # Get user input
     user_input = st.text_input("User Input")
+    
+    if st.button('Send'):
+        # Send user input to the model and get the response
+        model_response = send_message(user_input)
+        
+        # Append user input and model response to the conversation history
+        conversation.append(f'User: {user_input}')
+        conversation.append(f'ChatGPT: {model_response}')
+        
+        # Display conversation history
+        for message in conversation:
+            st.write(message)
 
-    if st.button("Send"):
-        response = send_chat_request(user_input)
-        messages = response['choices'][0]['message']['content']
-        st.write("ChatGPT:", messages)
-
-if __name__ == "__main__":
+# Run the Streamlit app
+if __name__ == '__main__':
     main()
