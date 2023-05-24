8beb92c16f023c3365b3c7f82070a89e228812ea
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-24 13:15:25 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,45 @@
 import streamlit as st
-import spacy
+from pyarabic.araby import strip_tashkeel, is_arabicrange
 
 def analyze_sentence(sentence):
-    nlp = spacy.load("xx_ent_wiki_sm")
-    doc = nlp(sentence)
-
+    words = sentence.split()
     verb = None
     subject = None
     object = None
-    oblique = None
-    preposition = None
 
-    for token in doc:
-        if token.pos_ == "VERB":
-            verb = token.text
-        elif token.dep_ == "nsubj":
-            subject = token.text
-        elif token.dep_ == "obj":
-            object = token.text
-        elif token.dep_ == "obl":
-            oblique = token.text
-            preposition = token.head.text
+    for word in words:
+        # التأكد من أن الكلمة تحتوي على حروف عربية فقط
+        if is_arabicrange(word):
+            # إزالة التشكيل من الكلمة
+            word_stripped = strip_tashkeel(word)
+            
+            # فحص إذا كانت الكلمة فعلاً
+            if word_stripped in verb_list:
+                verb = word
+            # فحص إذا كانت الكلمة فاعلاً
+            elif word_stripped in subject_list:
+                subject = word
+            # فحص إذا كانت الكلمة مفعولاً به
+            elif word_stripped in object_list:
+                object = word
 
-    return verb, subject, object, oblique, preposition
+    return verb, subject, object
 
+# تحميل قوائم الأفعال والفواعل والمفاعيل
+verb_list = ["فعل1", "فعل2", "فعل3"]
+subject_list = ["فاعل1", "فاعل2", "فاعل3"]
+object_list = ["مفعول1", "مفعول2", "مفعول3"]
+
+# تكوين واجهة المستخدم باستخدام Streamlit
 def main():
     st.title("تحليل الجملة العربية")
     sentence = st.text_input("أدخل الجملة:")
 
     if sentence:
-        verb, subject, object, oblique, preposition = analyze_sentence(sentence)
+        verb, subject, object = analyze_sentence(sentence)
         st.write("الفعل:", verb)
         st.write("الفاعل:", subject)
         st.write("المفعول به:", object)
-        st.write("اسم المجرور:", oblique)
-        st.write("الحرف الجر:", preposition)
 
 if __name__ == "__main__":
     main()
