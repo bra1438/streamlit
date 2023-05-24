b8de935d5c5cea20c764df720e69614fdbf7f4f5
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-24 13:45:43 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,40 @@
 import streamlit as st
-import pyarabic.araby as araby
-import stanza
+import spacy
 
-def analyze_word(word):
-    # تحميل نموذج اللغة العربية من stanza
-    stanza.download('ar')
-    nlp = stanza.Pipeline('ar')
+def analyze_sentence(sentence):
+    # تحميل النموذج اللغوي العربي من spaCy
+    nlp = spacy.load("xx_ent_wiki_sm")
 
-    # تحويل الكلمة إلى شكل معرب
-    arabic_word = araby.tokenize(word)[0]
+    # تحليل الجملة باستخدام spaCy
+    doc = nlp(sentence)
 
-    # تحليل الاعراب باستخدام stanza
-    doc = nlp(arabic_word)
-    sentence = doc.sentences[0]
-    words = sentence.words
+    # استخراج المعلومات النحوية
+    sentence_analysis = []
 
-    # استخراج الاعراب للكلمة
-    for word_info in words:
-        if word_info.text == arabic_word:
-            return word_info.xpos
+    for token in doc:
+        token_info = {
+            "الكلمة": token.text,
+            "التصنيف النحوي": token.pos_,
+            "الوظيفة النحوية": token.dep_,
+            "التفاصيل الإضافية": token.tag_
+        }
+        sentence_analysis.append(token_info)
 
-    return None
+    return sentence_analysis
 
 # تكوين واجهة المستخدم باستخدام Streamlit
 def main():
-    st.title("تحليل اعراب الكلمات العربية")
-    word = st.text_input("أدخل الكلمة:")
+    st.title("تحليل الجملة العربية")
+    sentence = st.text_input("أدخل الجملة:")
 
-    if word:
-        analysis = analyze_word(word)
-        if analysis:
-            st.write("اعراب الكلمة:", analysis)
-        else:
-            st.write("تعذر تحليل الكلمة")
+    if sentence:
+        sentence_analysis = analyze_sentence(sentence)
+        for token_info in sentence_analysis:
+            st.write("---")
+            st.write("الكلمة:", token_info["الكلمة"])
+            st.write("التصنيف النحوي:", token_info["التصنيف النحوي"])
+            st.write("الوظيفة النحوية:", token_info["الوظيفة النحوية"])
+            st.write("التفاصيل الإضافية:", token_info["التفاصيل الإضافية"])
 
 if __name__ == "__main__":
     main()
