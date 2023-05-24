f14bd445eb3e35afcbb01a836d271aae99d7bf01
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-24 13:12:56 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,40 @@
 import streamlit as st
-from pyarabic.araby import strip_tashkeel
-import stanza
- 
-def analyze_sentence(sentence):
-    # تحميل نموذج تحليل الجمل العربية من مكتبة stanza
-    stanza.download("ar")
-    nlp = stanza.Pipeline(lang='ar', processors='tokenize, pos')
+import spacy
 
-    # تحليل الجملة
+def analyze_sentence(sentence):
+    nlp = spacy.load("xx_ent_wiki_sm")
     doc = nlp(sentence)
 
-    # استخراج المعلومات النحوية
     verb = None
     subject = None
     object = None
+    oblique = None
+    preposition = None
 
-    for sentence in doc.sentences:
-        for word in sentence.words:
-            if word.upos == 'VERB':
-                verb = strip_tashkeel(word.text)
-            elif word.deprel == 'nsubj':
-                subject = strip_tashkeel(word.text)
-            elif word.deprel == 'obj':
-                object = strip_tashkeel(word.text)
+    for token in doc:
+        if token.pos_ == "VERB":
+            verb = token.text
+        elif token.dep_ == "nsubj":
+            subject = token.text
+        elif token.dep_ == "obj":
+            object = token.text
+        elif token.dep_ == "obl":
+            oblique = token.text
+            preposition = token.head.text
 
-    return verb, subject, object
+    return verb, subject, object, oblique, preposition
 
-# تكوين واجهة المستخدم باستخدام Streamlit
 def main():
     st.title("تحليل الجملة العربية")
     sentence = st.text_input("أدخل الجملة:")
 
     if sentence:
-        verb, subject, object = analyze_sentence(sentence)
+        verb, subject, object, oblique, preposition = analyze_sentence(sentence)
         st.write("الفعل:", verb)
         st.write("الفاعل:", subject)
         st.write("المفعول به:", object)
+        st.write("اسم المجرور:", oblique)
+        st.write("الحرف الجر:", preposition)
 
 if __name__ == "__main__":
     main()
