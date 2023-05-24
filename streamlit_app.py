5f4d921b16113dde2f85d5773670e91724e8eb0c
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-24 14:08:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,33 @@
 import streamlit as st
-from pyarabic.araby import morphological_analysis
+import pyarabic.araby as araby
 
-def analyze_verb_subject(sentence):
-    # استخراج الفعل والفاعل باستخدام PyArabic
-    analysis = morphological_analysis(sentence)
-    
+def analyze_sentence(sentence):
     verb = None
     subject = None
 
-    for word in analysis:
-        if 'فعل' in word['pos']:
-            verb = word['word']
-        elif 'اسم' in word['pos']:
-            subject = word['word']
+    # تفكيك الجملة إلى كلمات
+    words = araby.tokenize(sentence)
+
+    # البحث عن الفعل والفاعل
+    for i in range(len(words)):
+        if araby.is_arabicrange(words[i]):
+            # التأكد من أن الكلمة الحالية هي فعل
+            if araby.strip_tashkeel(words[i]) in ['فعل', 'مضارع', 'أمر', 'ماضي']:
+                verb = words[i]
+                # التأكد من أن الكلمة التالية هي فاعل
+                if i+1 < len(words) and araby.is_arabicrange(words[i+1]):
+                    subject = words[i+1]
+                break
 
     return verb, subject
 
 # تكوين واجهة المستخدم باستخدام Streamlit
 def main():
-    st.title("تحليل الفعل والفاعل العربي")
+    st.title("تحليل الجملة العربية")
     sentence = st.text_input("أدخل الجملة:")
 
     if sentence:
-        verb, subject = analyze_verb_subject(sentence)
+        verb, subject = analyze_sentence(sentence)
         st.write("الفعل:", verb)
         st.write("الفاعل:", subject)
 
