5a90075f5c2acb27da68dcc4967aaf6d4b62e2c7
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-24 13:39:07 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,34 @@
 import streamlit as st
-import spacy
+from pyarabic.araby import tokenize
 
 def analyze_sentence(sentence):
-    # تحميل النموذج اللغوي العربي من spaCy
-    nlp = spacy.load("xx_ent_wiki_sm")
+    # تقسيم الجملة إلى كلمات باستخدام pyarabic
+    words = tokenize(sentence)
 
-    # تحليل الجملة باستخدام spaCy
-    doc = nlp(sentence)
-
-    # استخراج المعلومات النحوية
+    # البحث عن الفعل والفاعل والمفعول به
     verb = None
     subject = None
     object = None
 
-    for token in doc:
-        if token.pos_ == "VERB":
-            verb = token.text
-        elif token.dep_ == "nsubj":
-            subject = token.text
-        elif token.dep_ == "obj":
-            object = token.text
+    for i in range(len(words)):
+        word = words[i]
+        if word.startswith("فعل"):
+            verb = word[3:]
+            # البحث عن الفاعل والمفعول به إذا كانا موجودين
+            if i > 0 and words[i-1].startswith("فاعل"):
+                subject = words[i-1][4:]
+            if i < len(words)-1 and words[i+1].startswith("مفعول"):
+                object = words[i+1][5:]
 
     return verb, subject, object
 
 # تكوين واجهة المستخدم باستخدام Streamlit
 def main():
-    st.title("تحليل الاعراب للجملة العربية")
+    st.title("تحليل الجملة العربية")
     sentence = st.text_input("أدخل الجملة:")
 
     if sentence:
         verb, subject, object = analyze_sentence(sentence)
-        st.write("الجملة:", sentence)
         st.write("الفعل:", verb)
         st.write("الفاعل:", subject)
         st.write("المفعول به:", object)
