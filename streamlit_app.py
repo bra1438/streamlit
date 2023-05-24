eeba31d274711869a181337598f5da64674bd45e
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-24 12:58:23 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,16 @@ import streamlit as st
 import pyarabic.araby as araby
 
 def ta3reeb(sentence):
-    words = araby.tokenize(sentence)
-    ta3reeb_sentence = ' '.join([araby.ta3reeb(word) for word in words])
-    return ta3reeb_sentence
+    words = sentence.split()
+    ta3reeb_sentence = []
+    for word in words:
+        ta3reeb_word = araby.tokenize(word)[0]
+        ta3reeb_sentence.append(ta3reeb_word)
+    return ' '.join(ta3reeb_sentence)
 
-# تكوين واجهة Streamlit
+# تهيئة واجهة Streamlit
 st.title("تعريب جملة عربية")
 sentence = st.text_input("أدخل الجملة:")
-
 if sentence:
     ta3reeb_sentence = ta3reeb(sentence)
     st.write("الجملة المعربة:", ta3reeb_sentence)
