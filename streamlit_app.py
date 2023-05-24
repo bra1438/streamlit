7d02d1b721c87c6938fddd632aca5aa7551a2e22
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-24 12:57:16 +0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,15 @@
 import streamlit as st
 import pyarabic.araby as araby
-    
-    import pyarabic.araby as araby
-from pyarabic.named import *
 
-def ta3reeb(word):
-    ta3reeb_word = araby.tokenize(word)[0]
-    return ta3reeb_word
+def ta3reeb(sentence):
+    words = araby.tokenize(sentence)
+    ta3reeb_sentence = ' '.join([araby.ta3reeb(word) for word in words])
+    return ta3reeb_sentence
 
-# إعداد واجهة المستخدم باستخدام Streamlit
+# تكوين واجهة Streamlit
 st.title("تعريب جملة عربية")
 sentence = st.text_input("أدخل الجملة:")
 
-if st.button("تعريب"):
-    words = sentence.split()
-    ta3reeb_sentence = []
-    for word in words:
-        ta3reeb_word = ta3reeb(word)
-        ta3reeb_sentence.append(ta3reeb_word)
-    ta3reeb_text = " ".join(ta3reeb_sentence)
-    st.write("الجملة المعربة:", ta3reeb_text)
-
-
-
-def ta3reeb_sentence(word):
-    tokens = araby.tokenize(word)  # تقسيم الجملة إلى كلمات
-    ta3reeb_sentence = ""
-
-    for token in tokens:
-        ta3reeb_word = ta3reeb(token)
-        ta3reeb_sentence += ta3reeb_word + " "
-
-    return ta3reeb_sentence.strip()
-
-# استخدام السكربت
-sentence = input("أدخل الجملة: ")
-ta3reeb_sentence = ta3reeb_sentence(word)
-print("الجملة المعربة:", ta3reeb_sentence)
- st.write("الجملة المعربة:", ta3reeb_sentence)
+if sentence:
+    ta3reeb_sentence = ta3reeb(sentence)
+    st.write("الجملة المعربة:", ta3reeb_sentence)
