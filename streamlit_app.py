047c3b2fca82c3aafaa054de067a02e347aa7f35
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 11:17:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,30 @@
 import streamlit as st
-import pyarabic.araby as araby
+import nltk
+from nltk.tag import tashaphyne
 
-def analyze_sentence(sentence):
-    # تحليل الجملة إلى كلمات منفردة
-    words = araby.tokenize(sentence)
-
-    # استخراج الفعل والفاعل
-    verb = None
-    subject = None
-
-    for word in words:
-        # التحقق مما إذا كانت الكلمة فعلًا
-        if araby.is_verb(word):
-            verb = word
-        # التحقق مما إذا كانت الكلمة فاعلًا
-        elif araby.is_noun(word):
-            subject = word
-
-    return verb, subject
+def pos_tag_arabic(text):
+    # تحميل قاموس الكلمات العربية
+    nltk.download("tashaphyne")
+    
+    # تقسيم النص إلى كلمات منفردة
+    words = nltk.word_tokenize(text)
+    
+    # إجراء POS tagging للنص العربي
+    pos_tags = tashaphyne.pos_tag(words)
+    
+    return pos_tags
 
 # تكوين واجهة المستخدم باستخدام Streamlit
 def main():
-    st.title("تحليل الجملة العربية")
-    sentence = st.text_input("أدخل الجملة:")
-
-    if sentence:
-        verb, subject = analyze_sentence(sentence)
-        st.write("الفعل:", verb)
-        st.write("الفاعل:", subject)
+    st.title("تحديد أجزاء الكلام في النص العربي")
+    text = st.text_area("أدخل النص:")
+    
+    if text:
+        pos_tags = pos_tag_arabic(text)
+        
+        st.write("نتائج تحديد أجزاء الكلام:")
+        for word, pos_tag in pos_tags:
+            st.write(f"{word}: {pos_tag}")
 
 if __name__ == "__main__":
     main()
