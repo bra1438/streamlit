1d58b2d7bc3dade178832670a94ae05a3e399ad7
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 09:12:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,22 @@
 import streamlit as st
 import pyarabic.araby as araby
+from pyarabic.named import *
 
 def analyze_sentence(sentence):
-    # تحليل الجملة لاستخراج الفعل والفاعل
+    # تحليل الجملة إلى كلمات منفردة
     words = araby.tokenize(sentence)
+
+    # استخراج الفعل والفاعل
     verb = None
     subject = None
 
-    for i in range(len(words)):
-        if araby.is_arabicrange(words[i]):
-            if araby.vocalized_verb(words[i]):
-                verb = words[i]
-                if i > 0 and araby.is_arabicrange(words[i-1]):
-                    subject = words[i-1]
-                elif i > 1 and araby.is_arabicrange(words[i-2]):
-                    subject = words[i-2]
-                break
+    for word in words:
+        # التحقق مما إذا كانت الكلمة فعلًا
+        if is_verb(word):
+            verb = word
+        # التحقق مما إذا كانت الكلمة فاعلًا
+        elif is_name(word):
+            subject = word
 
     return verb, subject
 
