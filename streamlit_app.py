678e706abd0c01f2c776016a6a3dabf4c1341a43
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 13:57:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,21 @@
 import streamlit as st
-from pyarabic import araby
-from pyarabic.araby import tokenize, is_arabicrange, strip_tashkeel
-from pyarabic.arabrepr import ArabRepr
-from pyarabic.number import vocalize
+from pyarabic.araby import tokenize
+from pyarabic.araby import morphological_analysis
 
 def analyze_sentence(sentence):
-    # تحليل النص
     tokens = tokenize(sentence)
-    # إعراب الجملة
-    repr = ArabRepr()
-    analysis = []
-    for token in tokens:
-        # تحويل الأعداد إلى كلمات
-        if is_arabicrange(token):
-            token = vocalize(token)
-        # إزالة التشكيل
-        token = strip_tashkeel(token)
-        # الإعراب
-        arabic_word = araby.tokenize(token)
-        analysis.append((token, repr.repr(arabic_word[0])))
-
+    analysis = [morphological_analysis(token) for token in tokens]
     return analysis
 
 def main():
-    st.title("تحليل إعراب الجمل العربية")
-    sentence = st.text_input("أدخل الجملة العربية:")
+    st.title("تحليل إعراب الجملة")
+
+    sentence = st.text_input("أدخل الجملة:")
     if sentence:
         analysis = analyze_sentence(sentence)
-        for word, analysis_result in analysis:
-            st.write(f"{word}: {analysis_result}")
+        st.write("نتائج التحليل:")
+        for token, analysis in zip(tokenize(sentence), analysis):
+            st.write(f"{token}: {analysis}")
 
 if __name__ == "__main__":
     main()
