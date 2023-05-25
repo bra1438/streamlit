3febdc996634243bc03ebbfd1352ca3a17a282fb
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 11:25:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,30 @@
 import streamlit as st
-import nltk
-from nltk.tokenize import word_tokenize
-import arabic_reshaper
-from bidi.algorithm import get_display
-from pyarabic.araby import strip_tashkeel
-from pyarabic import araby
+import stanza
 
-def pos_tag_arabic(sentence):
-    # تفريق الجملة إلى كلمات منفردة
-    words = word_tokenize(sentence)
+def pos_tag_arabic(text):
+    # Load the Arabic pre-trained POS tagger
+    nlp = stanza.Pipeline(lang='ar', processors='pos')
 
-    # إجراء POS tagging باستخدام NLTK
-    tagged_words = nltk.pos_tag(words)
+    # Process the text and perform POS tagging
+    doc = nlp(text)
 
-    # إعادة تشكيل الكلمات العربية
-    reshaped_words = [get_display(arabic_reshaper.reshape(word)) for word, _ in tagged_words]
+    pos_tags = []
+    for sentence in doc.sentences:
+        for word in sentence.words:
+            pos_tags.append((word.text, word.upos))
 
-    # إزالة التشكيل من الكلمات
-    stripped_words = [strip_tashkeel(word) for word in reshaped_words]
+    return pos_tags
 
-    # ترجيع الكلمات والتاغات
-    tags = [tag for _, tag in tagged_words]
-    return stripped_words, tags
-
-# تكوين واجهة المستخدم باستخدام Streamlit
+# Configure the Streamlit app
 def main():
-    st.title("تحليل POS للجملة العربية")
-    sentence = st.text_input("أدخل الجملة:")
+    st.title("Arabic Part-of-Speech Tagging")
+    text = st.text_area("Enter Arabic text:")
 
-    if sentence:
-        words, tags = pos_tag_arabic(sentence)
-        for word, tag in zip(words, tags):
-            st.write("الكلمة:", word)
-            st.write("التاغ:", tag)
+    if text:
+        pos_tags = pos_tag_arabic(text)
+        for word, pos_tag in pos_tags:
+            st.write("Word:", word)
+            st.write("POS Tag:", pos_tag)
             st.write("---")
 
 if __name__ == "__main__":
