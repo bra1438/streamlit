43fe2e7203bcdf3042e4a96a624141b5bdfb2f8d
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 14:41:13 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,28 @@
 import streamlit as st
+from pyarabic.araby import vocalized_word
 from pyarabic.araby import tokenize
-from pyarabic.araby import morphological_analysis
+from pyarabic.araby import strip_tashkeel
+from pyarabic.araby import is_arabicrange
 
 def analyze_sentence(sentence):
     tokens = tokenize(sentence)
-    analysis = [morphological_analysis(token) for token in tokens]
-    return analysis
+    analyzed_sentence = ""
+    for token in tokens:
+        if is_arabicrange(token):
+            token = strip_tashkeel(token)
+            token = vocalized_word(token)
+        analyzed_sentence += token + " "
+    return analyzed_sentence.strip()
+
 
 def main():
-    st.title("تحليل إعراب الجملة")
+    st.title("تطبيق الإعراب")
 
-    sentence = st.text_input("أدخل الجملة:")
-    if sentence:
-        analysis = analyze_sentence(sentence)
-        st.write("نتائج التحليل:")
-        for token, analysis in zip(tokenize(sentence), analysis):
-            st.write(f"{token}: {analysis}")
+    sentence = st.text_input("أدخل الجملة العربية:")
+    if st.button("إعراب"):
+        analyzed_sentence = analyze_sentence(sentence)
+        st.write("الجملة العربية المعربة:")
+        st.write(analyzed_sentence)
 
 if __name__ == "__main__":
     main()
