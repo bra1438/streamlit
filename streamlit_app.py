d497efbe6022e1938021528e79140a7d0015be53
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 13:28:35 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,31 @@
 import streamlit as st
-import openai
+from pyarabic.araby import is_arabicrange
+from pyarabic.araby import strip_tashkeel
+from pyarabic.araby import is_tashkeel
+from pyarabic.araby import simplify_tashkeel
+from pyarabic.araby import is_arabicletter
+from pyarabic.araby import is_arabicsymbol
 
-# Set up OpenAI API credentials
-openai.api_key = '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'
+def arabic_syntax():
+    st.title("تحليل واعراب الجمل العربية")
 
-# Set up Streamlit layout
-st.title("Arabic Sentence Analysis")
+    sentence = st.text_input("أدخل الجملة العربية:")
 
-# Get user input
-sentence = st.text_input("Enter an Arabic sentence:")
+    if sentence:
+        # تقوم بتنظيف الجملة من الأشكال الزائدة
+        clean_sentence = strip_tashkeel(sentence)
 
-# Define the CHAGPT prompt
-prompt = f"""
-from transformers import AutoTokenizer, AutoModelForTokenClassification
+        # قم بعمل الاعراب على الجملة المنظفة هنا
+        # يمكنك استخدام أي مكتبة أو أداة لتنفيذ عملية الاعراب
 
-# Define the sentence
-sentence = "{sentence}"
+        # عرض النتائج
+        st.subheader("الجملة الأصلية:")
+        st.write(sentence)
 
-# Load the CHAGPT tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("mofawzy/chagpt")
+        st.subheader("الجملة المنظفة:")
+        st.write(clean_sentence)
 
-model = AutoModelForTokenClassification.from_pretrained("mofawzy/chagpt")
+        st.subheader("النتائج:")
+        # عرض نتائج عملية الاعراب هنا
 
-# Tokenize the sentence
-tokens = tokenizer.tokenize(sentence)
-
-# Classify the tokens
-inputs = tokenizer.encode(sentence, return_tensors="pt")
-
-outputs = model(inputs)[0].argmax(2)[0]
-
-# Map the labels to their corresponding tokens
-labels = [model.config.id2label[label] for label in outputs]
-
-# Combine tokens with labels
-token_labels = list(zip(tokens, labels))
-
-# Get the i'rab
-irab = " ".join([f"{token}/{label}" for token, label in token_labels])
-
-# Return the i'rab
-irab
-"""
-
-# Call the OpenAI API to execute the code
-response = openai.Completion.create(
-    engine="davinci-codex",
-    prompt=prompt,
-    max_tokens=200,
-    n=1,
-    stop=None,
-    temperature=0.5
-)
-
-# Get the result from the API response
-result = response.choices[0].text.strip()
-
-# Display the i'rab
-st.header("i'rab Analysis:")
-st.write(result)
+arabic_syntax()
