55e0ef71a45a616cb3cf90bd3753e8a2163483e9
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 11:43:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,43 @@
 import streamlit as st
-import pyarabic.araby as araby
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+from nltk.chunk import ne_chunk
 
-def parse_sentence(sentence):
-    words = araby.tokenize(sentence)
-    analysis = []
+def extract_entities(sentence):
+    # تقسيم الجملة إلى كلمات منفردة
+    words = word_tokenize(sentence)
 
-    for word in words:
-        if araby.is_arabicrange(word):
-            parsed_word = araby.strip_tashkeel(word)
-            analysis.append(f"{word}: {araby.tokenize(parsed_word)}")
+    # تحديد الأنواع اللغوية (POS tagging)
+    tagged_words = pos_tag(words)
 
-    return analysis
+    # التعرف على الكيانات المسماة (Named Entity Recognition)
+    entities = ne_chunk(tagged_words)
+
+    # استخراج الفعل والفاعل والمفعول به
+    verb = None
+    subject = None
+    object_ = None
+
+    for entity in entities:
+        if hasattr(entity, 'label') and entity.label() == 'ORGANIZATION':
+            object_ = ' '.join(word for word, pos in entity.leaves())
+        elif hasattr(entity, 'label') and entity.label() == 'PERSON':
+            subject = ' '.join(word for word, pos in entity.leaves())
+        elif hasattr(entity, 'label') and entity.label() == 'VERB':
+            verb = ' '.join(word for word, pos in entity.leaves())
+
+    return verb, subject, object_
 
 def main():
-    st.title("Simple Arabic Sentence Parsing")
+    st.title("Extraction of Verb, Subject, and Object in Arabic Sentence")
     sentence = st.text_input("Enter an Arabic sentence:")
 
     if sentence:
-        analysis = parse_sentence(sentence)
-        for item in analysis:
-            st.write(item)
+        verb, subject, object_ = extract_entities(sentence)
+        st.write("Verb:", verb)
+        st.write("Subject:", subject)
+        st.write("Object:", object_)
 
 if __name__ == "__main__":
     main()
