a7dcf235b0ff333e767aeff12ee76b45eb8a2049
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 12:56:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,26 @@
 import streamlit as st
-import pyarabic.arabrepr as arabrepr
-import pyarabic.araby as araby
-from pyarabic.named import *
-from pyarabic.araby import strip_tashkeel
+from transformers import pipeline
 
+# Set up Streamlit layout
+st.title("Arabic Sentence Analysis")
 
-def find_verb_subject(sentence):
-    words = araby.tokenize(sentence)
+# Get user input
+sentence = st.text_input("Enter an Arabic sentence:")
 
-    verb = None
-    subject = None
+# Load the CHAGPT pipeline for morphological analysis
+morph_pipeline = pipeline(
+    "text2text-generation",
+    model="mofawzany/chagpt-mgb",
+    tokenizer="mofawzany/chagpt-mgb"
+)
 
-    for word in words:
-        root_word = strip_tashkeel(word)
-        analysis = arabrepr.ArabicRepresentations(root_word)
+# Perform morphological analysis and extract the i'rab
+def analyze_sentence(sentence):
+    result = morph_pipeline(sentence, max_length=100)[0]['generated_text']
+    return result
 
-        if analysis.is_verb():
-            verb = word
-        elif analysis.is_noun():
-            subject = word
-
-    return verb, subject
-
-def main():
-    st.title("Finding Verb and Subject in Arabic Sentence")
-    sentence = st.text_input("Enter an Arabic sentence:")
-
-    if sentence:
-        verb, subject = find_verb_subject(sentence)
-        st.write("Verb:", verb)
-        st.write("Subject:", subject)
-
-if __name__ == "__main__":
-    main()
+# Analyze the sentence and display the i'rab
+if sentence:
+    irab = analyze_sentence(sentence)
+    st.header("Morphological Analysis (i'rab):")
+    st.write(irab)
