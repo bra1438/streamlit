e13cf0a1b397b645b18b8dd24cd7f2ea2b870e19
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-25 13:01:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
-from transformers import pipeline
+import openai
+
+# Set up OpenAI API credentials
+openai.api_key = 'YOUR_API_KEY'
 
 # Set up Streamlit layout
 st.title("Arabic Sentence Analysis")
@@ -7,20 +10,52 @@ st.title("Arabic Sentence Analysis")
 # Get user input
 sentence = st.text_input("Enter an Arabic sentence:")
 
-# Load the CHAGPT pipeline for morphological analysis
-morph_pipeline = pipeline(
-    "text2text-generation",
-    model="mofawzany/chagpt-mgb",
-    tokenizer="mofawzany/chagpt-mgb"
+# Define the CHAGPT prompt
+prompt = f"""
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+
+# Define the sentence
+sentence = "{sentence}"
+
+# Load the CHAGPT tokenizer and model
+tokenizer = AutoTokenizer.from_pretrained("mofawzy/chagpt")
+
+model = AutoModelForTokenClassification.from_pretrained("mofawzy/chagpt")
+
+# Tokenize the sentence
+tokens = tokenizer.tokenize(sentence)
+
+# Classify the tokens
+inputs = tokenizer.encode(sentence, return_tensors="pt")
+
+outputs = model(inputs)[0].argmax(2)[0]
+
+# Map the labels to their corresponding tokens
+labels = [model.config.id2label[label] for label in outputs]
+
+# Combine tokens with labels
+token_labels = list(zip(tokens, labels))
+
+# Get the i'rab
+irab = " ".join([f"{token}/{label}" for token, label in token_labels])
+
+# Return the i'rab
+irab
+"""
+
+# Call the OpenAI API to execute the code
+response = openai.Completion.create(
+    engine="davinci-codex",
+    prompt=prompt,
+    max_tokens=200,
+    n=1,
+    stop=None,
+    temperature=0.5
 )
 
-# Perform morphological analysis and extract the i'rab
-def analyze_sentence(sentence):
-    result = morph_pipeline(sentence, max_length=100)[0]['generated_text']
-    return result
+# Get the result from the API response
+result = response.choices[0].text.strip()
 
-# Analyze the sentence and display the i'rab
-if sentence:
-    irab = analyze_sentence(sentence)
-    st.header("Morphological Analysis (i'rab):")
-    st.write(irab)
+# Display the i'rab
+st.header("i'rab Analysis:")
+st.write(result)
