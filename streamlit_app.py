7e8c85dfe6a16788eefffd8213c6fc901c26ceab
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-28 13:46:22 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,30 @@
-import speech_recognition as sr
 import streamlit as st
 
-def speech_recognition():
-    # Create a recognizer object
-    r = sr.Recognizer()
-    
-    # Use the microphone as the audio source
-    with sr.Microphone() as source:
-        
-        # Adjust for ambient noise
-        r.adjust_for_ambient_noise(source, duration=1)
-        
-        # Prompt the user to say something
-        st.write("Say something!")
-        
-        # Listen for speech input
-        audio = r.listen(source)
-        
-    # Use Google's speech recognition API to transcribe the speech
-    try:
-        text = r.recognize_google(audio)
-        st.write("You said: ", text)
-        
-    except sr.UnknownValueError:
-        st.write("Sorry, I couldn't understand what you said.")
-        
-    except sr.RequestError as e:
-        st.write("Sorry, something went wrong with the speech recognition service.")
-        
+# Dictionary of nutrition tips
+tips = {
+    "Tip 1": "Make half your plate fruits and vegetables for every meal.",
+    "Tip 2": "Choose whole grains instead of refined grains.",
+    "Tip 3": "Limit your intake of sugary drinks, such as soda and sports drinks.",
+    "Tip 4": "Choose lean protein sources, such as poultry and fish.",
+    "Tip 5": "Limit your intake of saturated fats and trans fats.",
+    "Tip 6": "Drink plenty of water throughout the day.",
+}
+
 # Create a Streamlit application
 def app():
-    st.title("Speech Recognition")
-
-    # Call the speech_recognition function on button click
-    if st.button("Speak"):
-        speech_recognition()
+    st.title("Nutrition Tips")
+    
+    # Display a random nutrition tip
+    rand_tip = st.button("Get Random Tip")
+    if rand_tip:
+        tip = random.choice(list(tips.values()))
+        st.success(tip)
+    
+    # Display all the nutrition tips
+    st.write("All Tips:")
+    for key, value in tips.items():
+        st.write(f"{key}: {value}")
+        
 
 # Run the Streamlit application
 if __name__ == "__main__":
