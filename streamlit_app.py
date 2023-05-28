1b3ca230c871b2083e28880a0ffdb36bc6b53b96
Brahim <32096282+bra1438@users.noreply.github.com>
2023-05-28 14:31:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,36 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 from PIL import Image
 
-# Create a Streamlit application
-def app():
-    st.title("Image Merger")
-    
-    # Select two images to merge
-    img1 = st.file_uploader("Choose the first image", type=["jpg", "jpeg", "png"])
-    img2 = st.file_uploader("Choose the second image", type=["jpg", "jpeg", "png"])
-    
-    # Merge the images and display the result
-    if img1 and img2:
-        # Load the images
-        image1 = Image.open(img1)
-        image2 = Image.open(img2)
-        
-        # Resize the images to the same size
-        size = (min(image1.size[0], image2.size[0]), min(image1.size[1], image2.size[1]))
-        image1 = image1.resize(size)
-        image2 = image2.resize(size)
-        
-        # Merge the images
-        im_merge = Image.blend(image1, image2, alpha=0.5)
-        
-        # Display the merged image
-        st.image(im_merge, use_column_width=True)
-        
-# Run the Streamlit application
-if __name__ == "__main__":
-    app()
+# Load model and labels
+model = tf.keras.models.load_model('traffic_signs_model.h5')
+labels = pd.read_csv('labels.csv')
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.title("Traffic Signs Classification")
+
+# Upload image
+uploaded_file = st.file_uploader("Choose an image", type=['jpg', 'jpeg', 'png'])
+
+if uploaded_file is not None:
+    # Load image
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image', use_column_width=True)
+
+    # Resize image and preprocess
+    image = image.resize((30, 30))
+    image = np.array(image)
+    image = image/255.0
+    image = image.reshape(1, 30, 30, 3)
+
+    # Make prediction
+    prediction = model.predict([image])
+    prediction = np.argmax(prediction)
+
+    # Display label
+    st.write("")
+    st.write("Classifying...")
+    label = labels.loc[prediction, 'SignName']
+    st.success(f"The traffic sign is {label}.")
